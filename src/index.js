357e811df2532e2fa6f178cfc54427d0f177cd2d
Anthony-Mbaya <120335962+Anthony-mbaya@users.noreply.github.com>
2024-02-29 13:35:17 +0300

    all files uploaded

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';  
+import Quote from './Quote'; 
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode> 
+    <Quote />
+  </React.StrictMode>
+); 
