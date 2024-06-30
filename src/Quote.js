2eacb4d6d6f50314c127ba62dc48f414193af60c
Anthony <anthonymwakulila@gmail.com>
2024-06-30 16:03:12 +0300

    modified style to be responsive

--- src/Quote.js
+++ src/Quote.js
@@ -1,29 +1,52 @@
-import React from 'react';
-import { useState } from 'react';
-import './Quote.css';
+import React from "react";
+import { useState } from "react";
+import "./Quote.css";
 
-function Quote(){
-    const [quote, setQuote] = useState('');
-    const [backgroundColor, setBackgroundColor] = useState('red');
-    const fetchApiFun =()=>{
-      setTimeout(() =>
-        fetch('https://sv443.net/jokeapi/v2/joke/Programming?type=single')
-        .then(response => response.json())  
-        .then((data) => setQuote(data.joke)) ,1000); 
-        const colors = ['#000080','#228B22','#800000','#333333','#00008B','#4B0082','#008080','#696969','#4B0082','#032923','#D2691E','#800000','#614051','#C04000','#808000'];
-        let randomColor = Math.floor(Math.random() * colors.length);
-        const selectedColor = colors[randomColor];
-        setTimeout(() => setBackgroundColor(selectedColor),1000);
-    }
-    return( 
-        <>
-        <button onClick={fetchApiFun} className='btn' >Generate Quote</button>
-        <div style={{color:'#c2bfbf', backgroundColor:backgroundColor}}  className="quote-box">
-          <h1>Generate joke quotes from joke API</h1>
-          <p>{quote}</p> 
-        </div>
-        </>
-    )  
-    
+export const Quote =()=> {
+  const [quote, setQuote] = useState("");
+  const [backgroundColor, setBackgroundColor] = useState("red");
+  const fetchApiFun = () => {
+    setTimeout(
+      () =>
+        fetch("https://sv443.net/jokeapi/v2/joke/Programming?type=single")
+          .then((response) => response.json())
+          .then((data) => setQuote(data.joke)),
+      1000
+    );
+    const colors = [
+      "#000080",
+      "#228B22",
+      "#800000",
+      "#333333",
+      "#00008B",
+      "#4B0082",
+      "#008080",
+      "#696969",
+      "#4B0082",
+      "#032923",
+      "#D2691E",
+      "#800000",
+      "#614051",
+      "#C04000",
+      "#808000",
+    ];
+    let randomColor = Math.floor(Math.random() * colors.length);
+    const selectedColor = colors[randomColor];
+    setTimeout(() => setBackgroundColor(selectedColor), 1000);
+  };
+  return (
+    <div className="cont">
+      <button onClick={fetchApiFun} className="btn">
+        Generate Quote
+      </button>
+      <div
+        style={{ color: "#c2bfbf", backgroundColor: backgroundColor }}
+        className="quote-box"
+      >
+        <h1>Generate joke quotes from joke API</h1>
+        <p>{quote}</p>
+      </div>
+    </div>
+  );
 }
 export default Quote;
